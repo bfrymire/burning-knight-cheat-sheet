a1bce05abffbf2619e6c1ff48f8ea36b814651bc
Brent Frymire <bfrymire22@gmail.com>
2023-05-24 18:27:19 -0400

    Disable search

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,9 +5,9 @@ export default function Header() {
 
     return (
         <div className="w-full h-12 text-sm flex justify-end drop-shadow-md">
-            <form onSubmit={handleOnSubmit}>
+            {/* <form onSubmit={handleOnSubmit}>
                 <input className="h-full bg-slate-200 p-5 text-black focus:outline-none" type="text" placeholder="Search for Anything!" />
-            </form>
+            </form> */}
         </div>
     );
 }
