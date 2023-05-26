dc97fcca33a06f15d147b4963c5e46f9a7b5e68c
Brent Frymire <bfrymire22@gmail.com>
2023-05-26 12:24:45 -0400

    Add BKCS logo and move item search to its own component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,13 @@
-export default function Header() {
-    const handleOnSubmit = (e) => {
-        e.preventDefault();
-    }
+import bkcs_logo from "../assets/image/bkcs_logo.png";
 
+export default function Header() {
     return (
-        <div className="w-full h-12 text-sm flex justify-end drop-shadow-md">
-            {/* <form onSubmit={handleOnSubmit}>
-                <input className="h-full bg-slate-200 p-5 text-black focus:outline-none" type="text" placeholder="Search for Anything!" />
-            </form> */}
+        <div className="w-full h-12 text-sm flex drop-shadow-md px-4">
+            <div className="p-2">
+                <a href="/">
+                    <img className="max-h-full" src={bkcs_logo} alt="Burning Knight Cheat Sheet Logo" />
+                </a>
+            </div>
         </div>
     );
 }
