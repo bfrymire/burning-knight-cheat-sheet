a7d081bbf315450837cd74348405c6293a7b6b01
Brent Frymire <bfrymire22@gmail.com>
2023-05-28 13:28:52 -0400

    Add item floating animation

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,7 +1,8 @@
 import ItemText from "./ItemText";
 import { useSelector, useDispatch } from "react-redux";
 import { hideModal } from "../features/modal/modalSlice";
-import { motion } from "framer-motion";
+import { motion, cubicBezier } from "framer-motion";
+import itemShadow from "../assets/image/item_shadow.png";
 
 export default function Modal() {
     const show = useSelector((state) => state.modal.show);
@@ -20,11 +21,24 @@ export default function Modal() {
                 {item &&
                     <div className="flex gap-10">
                         <div className="">
-                            < ItemText item={item} />
+                            <ItemText item={item} />
                         </div>
-                        <div className="flex items-center justify-center">
+                        <div className="flex-col items-center justify-center">
                             <div className="relative">
-                                <img src={item.imageSource} alt={`Item ${item.name}`} />
+                                <motion.img
+                                    className="relative z-20"
+                                    src={item.imageSource}
+                                    alt={`Item ${item.name}`}
+                                    animate={{
+                                        rotate: [0, 20, 0, -20, 0],
+                                        y: [0, -175 * 0.25, 0]
+                                    }}
+                                    transition={{
+                                        duration: 2,
+                                        repeat: Infinity,
+                                    }}
+                                />
+                                <img className="mx-auto opacity-50" src={itemShadow} alt="Item Shadow" />
                             </div>
                         </div>
                     </div>
